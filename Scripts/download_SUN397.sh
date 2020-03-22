71f7150139af0f9c285e83d77334d4dda0af3273
Prajwal Dhatwalia <dh.prajwal@gmail.com>
2020-03-22 04:19:45 -0400

    Corrected Folder Paths

--- Scripts/download_SUN397.sh
+++ Scripts/download_SUN397.sh
@@ -9,6 +9,6 @@ unzip -n $DATASET_DIR/SUN.zip -d $DATASET_DIR
 rm $DATASET_DIR/SUN.zip
 
 echo ========================================================================
-echo "Set the path below to \"ROOT:\" in the config file from config/config_SUN397.yaml:"
+echo "Set the path below to \"ROOT:\" in the config file from Config/config_SUN397.yaml:"
 echo -e "\033[32m $DATASET_DIR \033[00m"
 echo ========================================================================
